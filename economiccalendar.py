c4496c1df22dfe1d78e36539cf584c94809261b4
hauptkern <65733948+hauptkern@users.noreply.github.com>
2022-05-19 13:38:45 +0300

    Adding User Agent to economiccalendar.py requests to bypass bot detection

--- economiccalendar.py
+++ economiccalendar.py
@@ -13,7 +13,8 @@ class economiccalendar():
                 return proximity.get(x)
     @classmethod
     def getcalendar(self) -> list:
-        r=requests.get("https://tradingeconomics.com/calendar")
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+        r=requests.get("https://tradingeconomics.com/calendar",headers=headers)
         soup=BeautifulSoup(r.content,'html.parser')
         table=soup.find("table",attrs={"id":"calendar"})
         tableinner=str(table)
